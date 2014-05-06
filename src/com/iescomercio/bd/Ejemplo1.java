9fa617662a259d8e3f1ca3ced0d362cb0e27e683
ProfVespertino <ProfVespertino@ProfesorC7>
2014-05-06 18:29:53 +0200

    Ejemplo de acceso a BD - Primer ejemplo - Primer Commit

---
+++ src/com/iescomercio/bd/Ejemplo1.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iescomercio.bd;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ProfVespertino
+ */
+public class Ejemplo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            // Carga de forma dinámica el driver de Oracle
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            
+            // Establece la conexión
+            Connection con = DriverManager.getConnection(
+                    //"jdbc:oracle:thin:@localhost:1521:XE [system on LIBRO]",
+                    "jdbc:oracle:thin:@localhost:1521:XE",
+                    "system",
+                    "edu1");
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from LIBRO.ALUM");
+
+            while (rs.next()) {
+                String x = rs.getString("NOMBRE");
+                int s = rs.getInt("EDAD");
+                String x2 = rs.getString("LOCALIDAD");
+                System.out.println("Nombre: " + x + " Edad: " + s + " Localidad: " + x2);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Ejemplo1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Ejemplo1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
